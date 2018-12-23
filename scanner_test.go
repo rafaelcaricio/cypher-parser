6ffd198ca9a745d34a28aa72d78b257b1be29a9c
Rafael Caricio <rafael.caricio@fireeye.com>
2018-12-23 19:01:33 +0100

    Possible to read a stream of tokens

--- scanner_test.go
+++ scanner_test.go
@@ -1,6 +1,7 @@
 package cypher
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -43,6 +44,64 @@ func TestScanInput(t *testing.T) {
 			t.Errorf("For input `%s` expected token '%s' got '%s' (%s)", tc.in, tc.tok, tok, lit)
 		} else if lit != tc.lit {
 			t.Errorf("Expected literal '%s' got '%s'", tc.in, lit)
+		}
+	}
+}
+
+func TestScanMultiple(t *testing.T) {
+	type result struct {
+		tok Token
+		pos Pos
+		lit string
+	}
+	exp := []result{
+		{tok: MATCH, pos: Pos{Line: 0, Char: 0}, lit: ""},
+		{tok: WS, pos: Pos{Line: 0, Char: 5}, lit: " "},
+		{tok: LPAREN, pos: Pos{Line: 0, Char: 6}, lit: ""},
+		{tok: IDENT, pos: Pos{Line: 0, Char: 7}, lit: "n"},
+		{tok: COLON, pos: Pos{Line: 0, Char: 8}, lit: ""},
+		{tok: IDENT, pos: Pos{Line: 0, Char: 9}, lit: "Person"},
+		{tok: RPAREN, pos: Pos{Line: 0, Char: 15}, lit: ""},
+		{tok: WS, pos: Pos{Line: 0, Char: 16}, lit: " "},
+		{tok: WHERE, pos: Pos{Line: 0, Char: 17}, lit: ""},
+		{tok: WS, pos: Pos{Line: 0, Char: 22}, lit: " "},
+		{tok: IDENT, pos: Pos{Line: 0, Char: 23}, lit: "n"},
+		{tok: DOT, pos: Pos{Line: 0, Char: 24}, lit: ""},
+		{tok: IDENT, pos: Pos{Line: 0, Char: 25}, lit: "name"},
+		{tok: WS, pos: Pos{Line: 0, Char: 29}, lit: " "},
+		{tok: EQ, pos: Pos{Line: 0, Char: 30}, lit: ""},
+		{tok: WS, pos: Pos{Line: 0, Char: 31}, lit: " "},
+		{tok: STRING, pos: Pos{Line: 0, Char: 31}, lit: "Rafael"},
+		{tok: WS, pos: Pos{Line: 0, Char: 40}, lit: " "},
+		{tok: RETURN, pos: Pos{Line: 0, Char: 41}, lit: ""},
+		{tok: WS, pos: Pos{Line: 0, Char: 47}, lit: " "},
+		{tok: IDENT, pos: Pos{Line: 0, Char: 48}, lit: "n"},
+		{tok: EOF, pos: Pos{Line: 0, Char: 50}, lit: ""},
+	}
+
+	// Create a scanner.
+	v := `MATCH (n:Person) WHERE n.name = "Rafael" RETURN n`
+	s := NewScanner(strings.NewReader(v))
+
+	// Continually scan until we reach the end.
+	var act []result
+	for {
+		tok, pos, lit := s.Scan()
+		act = append(act, result{tok, pos, lit})
+		if tok == EOF {
+			break
+		}
+	}
+
+	// Verify the token counts match.
+	if len(exp) != len(act) {
+		t.Fatalf("token count mismatch: exp=%d, got=%d", len(exp), len(act))
+	}
+
+	// Verify each token matches.
+	for i := range exp {
+		if !reflect.DeepEqual(exp[i], act[i]) {
+			t.Fatalf("%d. token mismatch:\n\nexp=%#v\n\ngot=%#v", i, exp[i], act[i])
 		}
 	}
 }
